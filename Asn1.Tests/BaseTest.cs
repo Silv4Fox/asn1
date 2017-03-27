d6e208c159e02ae605f88ede0848b1898bc4c995
Henrik Cooke <henrik.cooke@ist.com>
2017-03-27 12:28:15 +0200

    Converted to .NET Standard.

--- Asn1.Tests/BaseTest.cs
+++ Asn1.Tests/BaseTest.cs
@@ -1,12 +1,13 @@
-﻿using NUnit.Framework;
+﻿using Xunit;
 
 namespace Asn1.Tests {
     public abstract class BaseTest {
 
-        public static void AreEqual(byte[] expected, byte[] actual) {
-            Assert.AreEqual(expected.Length, actual.Length);
-            for (var i = 0; i < expected.Length; i++) {
-                Assert.AreEqual(expected[i], actual[i]);
+        public static void Equal(byte[] expected, byte[] actual) {
+            Assert.Equal(expected.Length, actual.Length);
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
             }
         }
     }
